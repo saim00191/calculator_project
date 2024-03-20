bded750bc83c2491270abca7b86aa09655eb4b25
saim00191 <saimamjad198@gmail.com>
2024-03-21 03:21:22 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,52 @@
+#! /usr/bin/env node
+
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+const answers : {
+    NumberOne : number;
+    Numbertwo : number;
+    Operator : string
+} 
+ =await inquirer.prompt([
+    {
+        type:"number",
+        name:"NumberOne",
+        message:"Kindly Enter First Number :"
+    },
+    {
+        type:"number",
+        name:"Numbertwo",
+        message:"Kindly Enter Second Number :"
+    },
+    {
+        type:"list",
+        name:"Operator",
+        choices : ["Addition","Subtraction","Multiplication","Division","Exponent","Module"],
+        message : "Select Operator"
+    }
+])
+
+
+if(answers.NumberOne && answers.Numbertwo && answers.Operator){
+    let ans = 0
+if(answers.Operator === "Addition"){
+    ans = (answers.NumberOne + answers.Numbertwo);
+}else if(answers.Operator === "Subtraction"){
+   ans = (answers.NumberOne - answers.Numbertwo);
+}else if(answers.Operator === "Multiplication"){
+    ans = (answers.NumberOne * answers.Numbertwo);
+}else if(answers.Operator === "Division"){
+    ans = (answers.NumberOne / answers.Numbertwo);
+}else if(answers.Operator === "Exponent"){
+    ans =(answers.NumberOne ** answers.Numbertwo);
+}else if(answers.Operator === "Module"){
+    ans =(answers.NumberOne % answers.Numbertwo);
+}
+console.log(`Your Answer is `,ans);
+
+}
+else{
+    console.log(chalk.red("Invalid Input"));
+}
